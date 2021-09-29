0b9f00f7da36d324576331de545b944f22aa5027
leobyeon <ilbyeon@gmail.com>
2021-09-28 17:20:26 -0700

    implement useFlags instead of allFlags

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
 import { withLDProvider } from 'launchdarkly-react-client-sdk';
-import HelloWorld from './helloWorld';
-import { useLDClient } from 'launchdarkly-react-client-sdk';
+import { useLDClient, useFlags } from 'launchdarkly-react-client-sdk';
 
 function App() {
-  const ldClient = useLDClient();
+  // const ldClient = useLDClient();
+  const checkoutNewPuffsImage = useFlags();
+  console.log("checkoutNewPuffsImage", checkoutNewPuffsImage);
   return (
     <div className="App">
       <header className="App-header">
-        <code>{JSON.stringify(ldClient?.allFlags())}</code>
+        <code>{JSON.stringify(useFlags())}</code>
       </header>
     </div>
   );
 } 
 
-let clientID = '**INSERT-CLIENT-ID**'
+let clientID = '*CLIENT-ID-GOES-HERE*'
 export default withLDProvider({
   clientSideID: `${clientID}`,
   user: {
